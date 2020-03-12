388e81c74a89003bdd20f7ee695eb44340eace3d
fawzy-tat <ahmedfawzytat@gmail.com>
2020-03-11 20:24:16 -0700

    adding routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('Welcome to our express app');
+router.get("/", function(req, res, next) {
+  res.send("Welcome to our express app");
+});
+
+router.get("/lists/:users", function(req, res, next) {
+  // return status
+  if (!req.params.users) res.status(404).send("notfound");
+  //handle params
+  res.send(req.params.users);
+  //handle querystring
+  res.send(req.query);
 });
 
 module.exports = router;
