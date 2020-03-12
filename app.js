72bcda64d22ed74296185924ffa25f1eb5c8c69f
fawzy-tat <ahmedfawzytat@gmail.com>
2020-03-11 20:46:28 -0700

    disabling morgan logan for production

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(helmet());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-app.use(logger("dev"));
+if (process.env.NODE_ENV !== "production") app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
